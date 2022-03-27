416c2980f44c9c78be61b13ee1d977649b174631
Howard M. Harte <hharte@magicandroidapps.com>
2022-03-27 11:16:59 -0700

    Clean up formatting.

--- oasis_send.c
+++ oasis_send.c
@@ -1,22 +1,24 @@
-﻿/*
+/*
  * Utility to Send a file to an OASIS system via Serial Port.
  *
  * www.github.com/hharte/oasis-utils
  *
- * (c) 2021-2022, Howard M. Harte
+ * Copyright (c) 2021-2022, Howard M. Harte
  *
- * Reference: http://bitsavers.org/pdf/phaseOneSystems/oasis/Communications_Reference_Manual_Mar80.pdf pp. 8
+ * Reference:
+ * http://bitsavers.org/pdf/phaseOneSystems/oasis/Communications_Reference_Manual_Mar80.pdf
+ * pp. 8
  *
  */
 
 #define _CRT_SECURE_NO_DEPRECATE
 
 #ifdef _WIN32
-#include <windows.h>
-#else
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
+# include <windows.h>
+#else  /* ifdef _WIN32 */
+# include <string.h>
+# include <stdlib.h>
+# include <errno.h>
 #endif /* _WIN32 */
 
 #include <sys/stat.h>
@@ -24,44 +26,43 @@
 #include <stdint.h>
 #include <time.h>
 
-#include "oasis.h"
-#include "oasis_utils.h"
-#include "oasis_sendrecv.h"
-#include "mm_serial.h"
+#include "./oasis.h"
+#include "./oasis_utils.h"
+#include "./oasis_sendrecv.h"
+#include "./mm_serial.h"
 
 typedef struct oasis_send_args {
     char port_path[256];
-    int input_filename_index;
-    int ascii;
-    int quiet;
+    int  input_filename_index;
+    int  ascii;
+    int  quiet;
 } oasis_send_args_t;
 
-int parse_args(int argc, char* argv[], oasis_send_args_t* args);
-int oasis_send_packet(int fd, uint8_t* buf, uint16_t len, uint8_t cmd);
+int parse_args(int argc, char *argv[], oasis_send_args_t *args);
+int oasis_send_packet(int fd, uint8_t *buf, uint16_t len, uint8_t cmd);
 int oasis_wait_for_ack(int fd);
-int oasis_packet_encode(uint8_t* inbuf, uint16_t inlen, uint8_t* outbuf, uint16_t* outlen);
-int oasis_open_file(directory_entry_block_t* dir_entry, FILE** ostream, char* path, int quiet);
-int check_regular_file(const char* path);
+int oasis_packet_encode(uint8_t *inbuf, uint16_t inlen, uint8_t *outbuf, uint16_t *outlen);
+int oasis_open_file(directory_entry_block_t *dir_entry, FILE **ostream, char *path, int quiet);
+int check_regular_file(const char *path);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int fd;
     uint8_t commBuffer[1024] = { 0 };
     uint8_t decoded_buf[512];
     ssize_t bytes_written;
     ssize_t bytes_read;
-    directory_entry_block_t DirEntry = { 0 };
-    directory_entry_block_t* pDirEntry = &DirEntry;
-    FILE* istream;
+    directory_entry_block_t  DirEntry  = { 0 };
+    directory_entry_block_t *pDirEntry = &DirEntry;
+    FILE *istream;
     struct tm oasis_tm = { 0 };
-    int file_len = 0;
-    int toggle = 0;
+    int file_len       = 0;
+    int toggle         = 0;
     int positional_arg_cnt;
     oasis_send_args_t args;
     char fname_str[256];
     char ftype_str[256];
     char ffmt;
-    uint8_t* sequential_buf;
+    uint8_t *sequential_buf;
     int send_len;
     uint16_t frec_len;
 
@@ -71,40 +72,40 @@ int main(int argc, char *argv[])
         printf("OASIS Send Utility [%s] (c) 2021 - Howard M. Harte\n", VERSION);
         printf("https://github.com/hharte/oasis-utils\n\n");
 
-        printf("usage is: %s <port> [-q] [-a] <filename>|<path>\n", argv[0]);
+        printf("usage is: %s <port> [-q] [-a] <filename>|<path>\n",    argv[0]);
         printf("\t<port> Serial Port filename.\n");
         printf("\tFlags:\n");
         printf("\t      -q       Quiet: Don't list file details during extraction.\n");
         printf("\t      -a       ASCII: Convert line endings and truncate output file at EOF.\n");
-        return (-1);
+        return -1;
     }
 
     if ((fd = open_serial(args.port_path)) < 0) {
         printf("Error opening %s.\n", args.port_path);
-        return (-1);
+        return -1;
     }
 
     if (init_serial(fd, 9600) != 0) {
         printf("Error initializing %s.\n", args.port_path);
-        return (-1);
+        return -1;
     }
 
     for (int i = args.input_filename_index; i < argc; i++) {
         char send_filename[256];
-        int num_filled;
+        int  num_filled;
 
         strncpy(send_filename, argv[i], sizeof(send_filename) - 1);
         send_filename[sizeof(send_filename) - 1] = '\0';
 
-        if (check_regular_file(send_filename))
-        {
-            istream = fopen(send_filename, "rb");
+        if (check_regular_file(send_filename)) {
+            istream  = fopen(send_filename, "rb");
             send_len = 0;
             int retries = 20;
 
             send_filename[sizeof(send_filename) - 1] = '\0';
+
             for (int i = 0; i < strlen(send_filename); i++) {
-                if (send_filename[i] == '.' || send_filename[i] == '_') {
+                if ((send_filename[i] == '.') || (send_filename[i] == '_')) {
                     send_filename[i] = ' ';
                 }
             }
@@ -112,77 +113,82 @@ int main(int argc, char *argv[])
             num_filled = sscanf(send_filename, "%8s %8s %c %hu", fname_str, ftype_str, &ffmt, &frec_len);
 
             if (num_filled < 4) {
-                ffmt = 'S';
+                ffmt     = 'S';
                 frec_len = 0;
             }
 
+#if 0
             strcat(fname_str, "        ");
             strcat(ftype_str, "        ");
+#endif // 0
             memcpy(pDirEntry->file_name, fname_str, 8);
             memcpy(pDirEntry->file_type, ftype_str, 8);
 
             switch (ffmt) {
-            case 'D':
-            case 'd':
-                pDirEntry->file_format = FILE_FORMAT_DIRECT;
+                case 'D':
+                case 'd':
+                    pDirEntry->file_format = FILE_FORMAT_DIRECT;
 
-                fseek(istream, 0, SEEK_END);
-                file_len = ftell(istream);
-                fseek(istream, 0, SEEK_SET);
+                    fseek(istream, 0, SEEK_END);
+                    file_len = ftell(istream);
+                    fseek(istream, 0, SEEK_SET);
 
-                pDirEntry->record_count = (file_len + (frec_len - 1)) / frec_len;
-                pDirEntry->block_count = (file_len + 1023) / 1024;
-                pDirEntry->file_format_dependent1 = frec_len;
-                pDirEntry->file_format_dependent2 = 0;
-                break;
-            case 'S':
-            case 's':
-            default:
-                pDirEntry->file_format = FILE_FORMAT_SEQUENTIAL;
+                    pDirEntry->record_count           = (file_len + (frec_len - 1)) / frec_len;
+                    pDirEntry->block_count            = (file_len + 1023) / 1024;
+                    pDirEntry->file_format_dependent1 = frec_len;
+                    pDirEntry->file_format_dependent2 = 0;
+                    break;
+                case 'S':
+                case 's':
+                default:
+                    pDirEntry->file_format = FILE_FORMAT_SEQUENTIAL;
 
-                fseek(istream, 0, SEEK_END);
-                file_len = ftell(istream);
-                fseek(istream, 0, SEEK_SET);
+                    fseek(istream, 0, SEEK_END);
+                    file_len = ftell(istream);
+                    fseek(istream, 0, SEEK_SET);
 
-                pDirEntry->block_count = (file_len + 1023) / 1024;
+                    pDirEntry->block_count = (file_len + 1023) / 1024;
 
-                sequential_buf = calloc((size_t)pDirEntry->block_count * 1024, sizeof(uint8_t));
-                memset(sequential_buf, SUB, sizeof(uint8_t) * pDirEntry->block_count * 1024);
+                    sequential_buf = calloc((size_t)pDirEntry->block_count * 1024, sizeof(uint8_t));
+                    memset(sequential_buf, SUB, sizeof(uint8_t) * pDirEntry->block_count * 1024);
 
-                int line_count = 0;
-                int max_len = 0;
-                int cur_len = 0;
-                int prev_cr = 0;
-                while (!feof(istream)) {
-                    int c;
-                    c = fgetc(istream);
-                    if (args.ascii && prev_cr && c == LF) {
-                        /* Remove LF following CR */
-                        prev_cr = 0;
-                        continue;
+                    int line_count = 0;
+                    int max_len    = 0;
+                    int cur_len    = 0;
+                    int prev_cr    = 0;
+
+                    while (!feof(istream)) {
+                        int c;
+                        c = fgetc(istream);
+
+                        if (args.ascii && prev_cr && (c == LF)) {
+                            /* Remove LF following CR */
+                            prev_cr = 0;
+                            continue;
+                        }
+                        sequential_buf[send_len] = c;
+                        cur_len++;
+                        send_len++;
+
+                        if (c == CR) {
+                            if (cur_len > max_len) max_len = cur_len - 1;
+                            cur_len = 0;
+                            line_count++;
+                            prev_cr = 1;
+                        }
                     }
-                    sequential_buf[send_len] = c;
-                    cur_len++;
-                    send_len++;
-                    if (c == CR) {
-                        if (cur_len > max_len) max_len = cur_len - 1;
-                        cur_len = 0;
-                        line_count++;
-                        prev_cr = 1;
-                    }
-                }
 
-                pDirEntry->record_count = line_count;
-                pDirEntry->file_format_dependent1 = max_len;
-                pDirEntry->file_format_dependent2 = 0;
-                break;
+                    pDirEntry->record_count           = line_count;
+                    pDirEntry->file_format_dependent1 = max_len;
+                    pDirEntry->file_format_dependent2 = 0;
+                    break;
             }
 
             oasis_tm.tm_year = 91;
-            oasis_tm.tm_mon = 11;
+            oasis_tm.tm_mon  = 11;
             oasis_tm.tm_mday = 02;
             oasis_tm.tm_hour = 04;
-            oasis_tm.tm_min = 33;
+            oasis_tm.tm_min  = 33;
 
             oasis_convert_tm_to_timestamp(&oasis_tm, &pDirEntry->timestamp);
 
@@ -200,7 +206,7 @@ int main(int argc, char *argv[])
 
                 if (retries == 0) {
                     printf("\nTimeout waiting for sending station.\n");
-                    return (-2);
+                    return -2;
                 }
             } while (oasis_wait_for_ack(fd) == -1);
 
@@ -214,15 +220,14 @@ int main(int argc, char *argv[])
             oasis_wait_for_ack(fd);
 
             if (pDirEntry->file_format != FILE_FORMAT_SEQUENTIAL) {
-                while ((bytes_read = (uint32_t)fread(&decoded_buf, 1, BLOCK_SIZE, istream)))
-                {
+                while ((bytes_read = (uint32_t)fread(&decoded_buf, 1, BLOCK_SIZE, istream))) {
                     oasis_send_packet(fd, decoded_buf, (uint16_t)bytes_read, WRITE);
                     oasis_wait_for_ack(fd);
                 }
             } else { /* Sequential File */
                 uint16_t sector_cnt = 1;
-                for (int bytes_sent = 0; bytes_sent <= send_len; bytes_sent += BLOCK_SIZE-2)
-                {
+
+                for (int bytes_sent = 0; bytes_sent <= send_len; bytes_sent += BLOCK_SIZE - 2) {
                     printf("\rSegment: %d", sector_cnt);
                     memcpy(decoded_buf, &sequential_buf[bytes_sent], BLOCK_SIZE - 2);
                     decoded_buf[BLOCK_SIZE - 2] = sector_cnt >> 8;
@@ -253,9 +258,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-
-int parse_args(int argc, char* argv[], oasis_send_args_t* args)
-{
+int parse_args(int argc, char *argv[], oasis_send_args_t *args) {
     int positional_arg_cnt = 0;
 
     memset(args, 0, sizeof(oasis_send_args_t));
@@ -263,27 +266,27 @@ int parse_args(int argc, char* argv[], oasis_send_args_t* args)
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] != '-') {
             switch (positional_arg_cnt) {
-            case 0:
-                snprintf(args->port_path, sizeof(args->port_path), "%s", argv[i]);
-                break;
-            case 1:
-                args->input_filename_index = i;
-                break;
+                case 0:
+                    snprintf(args->port_path, sizeof(args->port_path), "%s", argv[i]);
+                    break;
+                case 1:
+                    args->input_filename_index = i;
+                    break;
             }
             positional_arg_cnt++;
-        }
-        else {
+        } else {
             char flag = argv[i][1];
+
             switch (flag) {
-            case 'a':
-                args->ascii = 1;
-                break;
-            case 'q':
-                args->quiet = 1;
-                break;
-            default:
-                printf("Unknown option '-%c'\n", flag);
-                break;
+                case 'a':
+                    args->ascii = 1;
+                    break;
+                case 'q':
+                    args->quiet = 1;
+                    break;
+                default:
+                    printf("Unknown option '-%c'\n", flag);
+                    break;
             }
         }
     }
@@ -291,12 +294,12 @@ int parse_args(int argc, char* argv[], oasis_send_args_t* args)
 }
 
 #if !defined(S_ISREG) && defined(S_IFMT) && defined(S_IFREG)
-#define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
-#endif
+# define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
+#endif /* if !defined(S_ISREG) && defined(S_IFMT) && defined(S_IFREG) */
 
-int check_regular_file(const char* path)
-{
+int check_regular_file(const char *path) {
     struct stat path_stat;
+
     stat(path, &path_stat);
     return S_ISREG(path_stat.st_mode);
 }
--- oasis_utils.c
+++ oasis_utils.c
@@ -3,19 +3,19 @@
  *
  * www.github.com/hharte/oasis
  *
- * (c) 2021, Howard M. Harte
+ * Copyright (c) 2021-2022, Howard M. Harte
  *
  * Reference: http://bitsavers.org/pdf/phaseOneSystems/oasis/
  *
  */
 
 #if defined(_WIN32)
-#define _CRT_SECURE_NO_DEPRECATE
-#include <windows.h>
-#else
-#include <sys/types.h>
-#include <utime.h>
-#endif
+# define _CRT_SECURE_NO_DEPRECATE
+# include <windows.h>
+#else  /* if defined(_WIN32) */
+# include <sys/types.h>
+# include <utime.h>
+#endif /* if defined(_WIN32) */
 
 #include <errno.h>
 #include <stdlib.h>
@@ -24,14 +24,15 @@
 #include <string.h>
 #include <time.h>
 
-#include "oasis.h"
+#include "./oasis.h"
+#include "./oasis_utils.h"
 
 const char kPathSeparator =
 #ifdef _WIN32
-'\\';
-#else
-'/';
-#endif
+    '\\';
+#else  /* ifdef _WIN32 */
+    '/';
+#endif /* ifdef _WIN32 */
 
 /* Packed timestamp:
  * +-------------------------------------------------+
@@ -40,172 +41,208 @@ const char kPathSeparator =
  * | BIT   | 7         0 | 7         0 | 7         0 |
  * +-------------------------------------------------+
  */
-void oasis_convert_timestamp_to_tm(oasis_tm_t *timestamp, struct tm *tmout)
-{
-	tmout->tm_mon = ((timestamp->raw[0] & 0xF0) >> 4) - 1;
-	tmout->tm_mday = ((timestamp->raw[0] & 0x0F) << 1) | ((timestamp->raw[1] & 0x80) >> 7);
-	tmout->tm_year = ((timestamp->raw[1] & 0x78) >> 3) + 77; /* 0 = 1977, highest valid year is 1992. */
-	tmout->tm_hour = ((timestamp->raw[1] & 0x07) << 2) | ((timestamp->raw[2] & 0xc0) >> 6);
-	tmout->tm_min = timestamp->raw[2] & 0x3F;
-	tmout->tm_sec = 0;
-	tmout->tm_isdst = 0;
+void oasis_convert_timestamp_to_tm(oasis_tm_t *timestamp, struct tm *tmout) {
+    tmout->tm_mon  = ((timestamp->raw[0] & 0xF0) >> 4) - 1;
+    tmout->tm_mday =
+        ((timestamp->raw[0] & 0x0F) << 1) | ((timestamp->raw[1] & 0x80) >> 7);
+
+    /* 0 = 1977, highest valid year is 1992. */
+    tmout->tm_year = ((timestamp->raw[1] & 0x78) >> 3) + 77;
+    tmout->tm_hour =
+        ((timestamp->raw[1] & 0x07) << 2) | ((timestamp->raw[2] & 0xc0) >> 6);
+    tmout->tm_min   = timestamp->raw[2] & 0x3F;
+    tmout->tm_sec   = 0;
+    tmout->tm_isdst = 0;
 }
 
-void oasis_convert_tm_to_timestamp(struct tm* tmin, oasis_tm_t* timestamp)
-{
-	timestamp->raw[0]  = ((tmin->tm_mon & 0xF) + 1) << 4;
-	timestamp->raw[0] |= ((tmin->tm_mday >> 1) & 0x0F);
-	timestamp->raw[1]  = ((tmin->tm_mday & 1) << 7);
-	timestamp->raw[1] |= ((tmin->tm_year - 77) & 0x0F) << 3;
-	timestamp->raw[1] |= ((tmin->tm_hour >> 2) & 0x07);
-	timestamp->raw[2]  = ((tmin->tm_hour & 0x03) << 6);
-	timestamp->raw[2] |= ((tmin->tm_min & 0x3F));
+void oasis_convert_tm_to_timestamp(struct tm *tmin, oasis_tm_t *timestamp) {
+    timestamp->raw[0]  = ((tmin->tm_mon & 0xF) + 1) << 4;
+    timestamp->raw[0] |= ((tmin->tm_mday >> 1) & 0x0F);
+    timestamp->raw[1]  = ((tmin->tm_mday & 1) << 7);
+    timestamp->raw[1] |= ((tmin->tm_year - 77) & 0x0F) << 3;
+    timestamp->raw[1] |= ((tmin->tm_hour >> 2) & 0x07);
+    timestamp->raw[2]  = ((tmin->tm_hour & 0x03) << 6);
+    timestamp->raw[2] |= ((tmin->tm_min & 0x3F));
 }
 
-void oasis_list_dir_entry(directory_entry_block_t* dir_entry)
-{
-	char fname[FNAME_LEN + 1];
-	char fext[FEXT_LEN + 1];
-	char format[8];
-	char other_str[8];
-	struct tm tmout;
-	char buf[17];
+void oasis_list_dir_entry(directory_entry_block_t *dir_entry) {
+    char fname[FNAME_LEN + 1];
+    char fext[FEXT_LEN + 1];
+    char format[8];
+    char other_str[8];
+    struct tm tmout;
+    char buf[17];
 
-	if (dir_entry->file_format == 0) {
-		return;
-	}
+    if (dir_entry->file_format == 0) {
+        return;
+    }
 
-	snprintf(fname, sizeof(fname), "%s", dir_entry->file_name);
-	snprintf(fext, sizeof(fext), "%s", dir_entry->file_type);
-	snprintf(other_str, sizeof(other_str), "       ");
+    snprintf(fname,     sizeof(fname),     "%s", dir_entry->file_name);
+    snprintf(fext,      sizeof(fext),      "%s", dir_entry->file_type);
+    snprintf(other_str, sizeof(other_str), "       ");
 
-	switch (dir_entry->file_format) {
-	case FILE_FORMAT_DELETED:
-		snprintf(format, sizeof(format), "Deleted");
-		break;
-	case FILE_FORMAT_EMPTY:
-		snprintf(format, sizeof(format), "Empty  ");
-		break;
-	case FILE_FORMAT_SYNONYM:
-		snprintf(format, sizeof(format), "Synonym");
-		break;
-	default:
-		switch (dir_entry->file_format & 0x1F) {
-		case FILE_FORMAT_RELOCATABLE:
-			snprintf(format, sizeof(format), "R %5d", dir_entry->file_format_dependent1);
-			if (dir_entry->file_format_dependent2 > 0) {
-				snprintf(other_str, sizeof(other_str), "%5d L", dir_entry->file_format_dependent2);
-			}
-			break;
-		case FILE_FORMAT_ABSOLUTE:
-			snprintf(format, sizeof(format), "A %5d", dir_entry->file_format_dependent1);
-			snprintf(other_str, sizeof(other_str), "%5d O", dir_entry->file_format_dependent2);
-			break;
-		case FILE_FORMAT_SEQUENTIAL:
-			snprintf(format, sizeof(format), "S %5d", dir_entry->file_format_dependent1);
-			if (dir_entry->file_format_dependent2 > 0) {
-				snprintf(other_str, sizeof(other_str), "%5d E", dir_entry->file_format_dependent2);
-			}
-			break;
-		case FILE_FORMAT_DIRECT:
-			snprintf(format, sizeof(format), "D %5d", dir_entry->file_format_dependent1);
-			break;
-		case FILE_FORMAT_INDEXED:
-			snprintf(format, sizeof(format), "I%3d/%3d", dir_entry->file_format_dependent1 & 0x1F, (dir_entry->file_format_dependent1 & 0xFE) >> 9);
-			break;
-		case FILE_FORMAT_KEYED:
-			snprintf(format, sizeof(format), "K%3d/%3d", dir_entry->file_format_dependent1 & 0x1F, (dir_entry->file_format_dependent1 & 0xFE) >> 9);
-			break;
-		default:
-			printf("Unsupported file type %x\n", dir_entry->file_format);
-			break;
-		}
-	}
+    switch (dir_entry->file_format) {
+        case FILE_FORMAT_DELETED:
+            snprintf(format, sizeof(format), "Deleted");
+            break;
+        case FILE_FORMAT_EMPTY:
+            snprintf(format, sizeof(format), "Empty  ");
+            break;
+        case FILE_FORMAT_SYNONYM:
+            snprintf(format, sizeof(format), "Synonym");
+            break;
+        default:
 
-	oasis_convert_timestamp_to_tm(&dir_entry->timestamp, &tmout);
-	strftime(buf, 17, "%m/%d/%y %H:%M", &tmout);
-	printf("%s %s %s %5d %4d %s %5d %3d %3d  %s\n",
-		fname,
-		fext,
-		buf,
-		dir_entry->record_count,
-		dir_entry->block_count,
-		format,
-		dir_entry->start_sector,
-		dir_entry->owner_id,
-		dir_entry->shared_from_owner_id,
-		other_str);
+            switch (dir_entry->file_format & 0x1F) {
+                case FILE_FORMAT_RELOCATABLE:
+                    snprintf(format,
+                             sizeof(format),
+                             "R %5d",
+                             dir_entry->file_format_dependent1);
+
+                    if (dir_entry->file_format_dependent2 > 0) {
+                        snprintf(other_str,
+                                 sizeof(other_str),
+                                 "%5d L",
+                                 dir_entry->file_format_dependent2);
+                    }
+                    break;
+                case FILE_FORMAT_ABSOLUTE:
+                    snprintf(format,
+                             sizeof(format),
+                             "A %5d",
+                             dir_entry->file_format_dependent1);
+                    snprintf(other_str,
+                             sizeof(other_str),
+                             "%5d O",
+                             dir_entry->file_format_dependent2);
+                    break;
+                case FILE_FORMAT_SEQUENTIAL:
+                    snprintf(format,
+                             sizeof(format),
+                             "S %5d",
+                             dir_entry->file_format_dependent1);
+
+                    if (dir_entry->file_format_dependent2 > 0) {
+                        snprintf(other_str,
+                                 sizeof(other_str),
+                                 "%5d E",
+                                 dir_entry->file_format_dependent2);
+                    }
+                    break;
+                case FILE_FORMAT_DIRECT:
+                    snprintf(format,
+                             sizeof(format),
+                             "D %5d",
+                             dir_entry->file_format_dependent1);
+                    break;
+                case FILE_FORMAT_INDEXED:
+                    snprintf(format,
+                             sizeof(format),
+                             "I%3d/%3d",
+                             dir_entry->file_format_dependent1 & 0x1F,
+                             (dir_entry->file_format_dependent1 & 0xFE) >> 9);
+                    break;
+                case FILE_FORMAT_KEYED:
+                    snprintf(format,
+                             sizeof(format),
+                             "K%3d/%3d",
+                             dir_entry->file_format_dependent1 & 0x1F,
+                             (dir_entry->file_format_dependent1 & 0xFE) >> 9);
+                    break;
+                default:
+                    printf("Unsupported file type %x\n", dir_entry->file_format);
+                    break;
+            }
+    }
+
+    oasis_convert_timestamp_to_tm(&dir_entry->timestamp, &tmout);
+    strftime(buf, 17, "%m/%d/%y %H:%M", &tmout);
+    printf("%s %s %s %5d %4d %s %5d %3d %3d  %s\n",
+           fname,
+           fext,
+           buf,
+           dir_entry->record_count,
+           dir_entry->block_count,
+           format,
+           dir_entry->start_sector,
+           dir_entry->owner_id,
+           dir_entry->shared_from_owner_id,
+           other_str);
 }
 
-void set_file_time(char* output_filename, struct tm* tmin)
-{
+void set_file_time(char *output_filename, struct tm *tmin) {
 #if defined(_WIN32)
-	FILETIME file_time;
-	SYSTEMTIME system_time;
-	HANDLE hOstream;
+    FILETIME   file_time;
+    SYSTEMTIME system_time;
+    HANDLE     hOstream;
 
-	GetSystemTime(&system_time);
+    GetSystemTime(&system_time);
 
-	system_time.wDay = tmin->tm_mday;
-	system_time.wMonth = tmin->tm_mon;
-	system_time.wYear = tmin->tm_year + 1900;
-	system_time.wHour = tmin->tm_hour;
-	system_time.wMinute = tmin->tm_min;
-	system_time.wSecond = 0;
+    system_time.wDay    = tmin->tm_mday;
+    system_time.wMonth  = tmin->tm_mon;
+    system_time.wYear   = tmin->tm_year + 1900;
+    system_time.wHour   = tmin->tm_hour;
+    system_time.wMinute = tmin->tm_min;
+    system_time.wSecond = 0;
 
-	SystemTimeToFileTime(&system_time, &file_time);
+    SystemTimeToFileTime(&system_time, &file_time);
 
-	hOstream = CreateFileA(output_filename,
-		FILE_WRITE_ATTRIBUTES, FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL, OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL, NULL);
-	SetFileTime(hOstream, &file_time, &file_time, &file_time);
+    hOstream = CreateFileA(output_filename,
+                           FILE_WRITE_ATTRIBUTES,
+                           FILE_SHARE_READ | FILE_SHARE_WRITE,
+                           NULL,
+                           OPEN_EXISTING,
+                           FILE_ATTRIBUTE_NORMAL,
+                           NULL);
+    SetFileTime(hOstream, &file_time, &file_time, &file_time);
 #else /* POSIX */
-	struct utimbuf file_time;
+    struct utimbuf file_time;
 
-	file_time.actime = mktime(tmin);
-	file_time.modtime = mktime(tmin);
+    file_time.actime  = mktime(tmin);
+    file_time.modtime = mktime(tmin);
 
-	if (utime(output_filename, &file_time)) {
-		fprintf(stderr, "Error setting timestamp for %s\n", output_filename);
-	}
-#endif
+    if (utime(output_filename, &file_time)) {
+        fprintf(stderr, "Error setting timestamp for %s\n", output_filename);
+    }
+#endif /* if defined(_WIN32) */
 }
 
-void dump_hex(uint8_t* data, int len)
-{
-	uint8_t ascii[32] = { 0 };
-	uint8_t* pascii = ascii;
+void dump_hex(uint8_t *data, int len) {
+    uint8_t  ascii[32] = { 0 };
+    uint8_t *pascii    = ascii;
 
-	printf("\n");
-	if (len > 0) {
-		uint16_t i;
-		printf("\tData:");
+    printf("\n");
 
-		for (i = 0; i < len; i++) {
-			if (i % 16 == 0) {
-				if (i > 0) {
-					*pascii++ = '\0';
-					printf("%s", ascii);
-				}
-				printf("\n\t%03x: ", i);
-				pascii = ascii;
-			}
-			printf("%02x, ", data[i]);
-			if (((data[i]) >= 0x20) && (data[i] < 0x7F)) {
-				*pascii++ = data[i];
-			}
-			else {
-				*pascii++ = '.';
-			}
-		}
-		*pascii++ = '\0';
-		if (strnlen((char*)ascii, sizeof(ascii)) > 0) {
-			for (i = 0; i < 16 - strnlen((char*)ascii, sizeof(ascii)); i++) {
-				printf("    ");
-			}
-			printf("%s", ascii);
-		}
-	}
-	printf("\n");
+    if (len > 0) {
+        uint16_t i;
+        printf("\tData:");
+
+        for (i = 0; i < len; i++) {
+            if (i % 16 == 0) {
+                if (i > 0) {
+                    *pascii++ = '\0';
+                    printf("%s", ascii);
+                }
+                printf("\n\t%03x: ", i);
+                pascii = ascii;
+            }
+            printf("%02x, ", data[i]);
+
+            if (((data[i]) >= 0x20) && (data[i] < 0x7F)) {
+                *pascii++ = data[i];
+            } else {
+                *pascii++ = '.';
+            }
+        }
+        *pascii++ = '\0';
+
+        if (strnlen((char *)ascii, sizeof(ascii)) > 0) {
+            for (i = 0; i < 16 - strnlen((char *)ascii, sizeof(ascii)); i++) {
+                printf("    ");
+            }
+            printf("%s", ascii);
+        }
+    }
+    printf("\n");
 }
